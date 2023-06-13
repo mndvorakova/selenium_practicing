e80eb80241b70acf02385f40735a434858f4eee1
mndvorakova <dvorakova.maja12@gmail.com>
2023-06-13 13:05:54 +0200

    first test: 1) Importing jar. files to module settings in IntelliJ IDEA 2) Configuration of chrome or gecko (firefox) driver and config path 3) Starting the first test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,16 +2,20 @@ package org.example;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\MajaNastassjaDvorako\\selenium_java\\chromedriver.exe");
 
+        ChromeOptions moznosti = new ChromeOptions();
+
+        //incognito mode
+        moznosti.addArguments("--incognito");
+
         //urcuji typ webdriveru + se nastartuje browser
-        WebDriver chrome = new ChromeDriver();
+        WebDriver chrome = new ChromeDriver(moznosti);
 
         Thread.sleep(2000);
 
@@ -21,8 +25,7 @@ public class Main {
         chrome.manage().window().maximize();
         Thread.sleep(2000);
 
-        chrome.quit();
-
+        //chrome.quit();
 
 
 
